4e43ecdd70282e5f67551d3be72985ce7597ddb9
ilan990 <77548546+ilan990@users.noreply.github.com>
2023-05-01 17:48:43 +0200

    Ajout d'une api qui va chercher les informations des films

--- Scrapping_imdb/README.md
+++ Scrapping_imdb/README.md
@@ -1,14 +1,23 @@
 # Extractions de données IMDB
-Ce programme recherche sur le site IMDB les meilleurs films de tous les temps, les enregistre dans un fichier CSV et crée un graphique qui montre le nombre de meilleurs films par dizaine d'années
+Ce programme :
+    recherche sur le site IMDB les meilleurs films de tous les temps, 
+    Les enregistre dans un fichier CSV 
+    Crée un graphique qui montre le nombre de meilleurs films par dizaine d'années
+    Appelle une api pour avoir les informations des films
+    Crée un graphique des 5 pays avec le plus de films dans le Top
 
 ## Dépendances
     Urllib
     Pandas
     BeautifulSoup
     Matplotlib
+    re
+    request
 
 ## Utilisation
 Exécutez le script et il commencera à chercher les 250 meilleures films pour les mettre dans un fichier de type CSV.
 
 ## Image
 ![graphique](graphique.png)
+![graphique](pays.png)
+
--- Scrapping_imdb/scrap.py
+++ Scrapping_imdb/scrap.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup as bs
 import urllib.request
 import pandas as pd
 import matplotlib.pyplot as plt
+import re
+import requests
 
 url = 'https://www.imdb.com/chart/top/?ref_=nv_mv_250'
 page = urllib.request.urlopen(url, timeout=5)
@@ -15,10 +17,17 @@ for tr in tbody.find_all('tr'):
     td = tr.find('td', {'class': 'titleColumn'})
     a = td.find('a')
     if a:
+        idMovie = re.search(r'(tt\d+)', str(a)).group(1)
         titre = a.text
-        date = td.find('span').text.strip('()')
-        film = {'Titre': titre, 'Date': date}
+
+        # Appel de l'api OMDB
+        url = 'http://www.omdbapi.com/?i=' + idMovie + '&apikey=b4d62f57'
+        response = requests.get(url)
+        donnee = response.json()
+        film = {'id': idMovie, 'Titre': titre, 'Date': donnee['Year'], 'Pays': donnee['Country'],
+                'Director': donnee['Director'], 'acteurs': donnee['Actors'], 'note': donnee['imdbRating']}
         listMovies.append(film)
+
 
 df = pd.DataFrame(listMovies)
 
@@ -37,7 +46,15 @@ plt.title('Nombre de meilleurs films par dizaine d\'années')
 plt.savefig('graphique.png')
 plt.show()
 
+df['Pays'] = df['Pays'].apply(lambda x: x.split(',')[0].strip())
+counts = df.groupby('Pays')['Titre'].count().nlargest(5)
+counts.plot(kind='bar', color='yellow', edgecolor='red', width=0.9)
+
+plt.xlabel('Pays')
+plt.ylabel('Quantité')
+plt.title('Nombre de meilleurs films par Pays')
+plt.savefig('pays.png')
+plt.show()
 
 # Enregistrer les données dans un fichier CSV
 df.to_csv('nombres.csv', index=False)
-
